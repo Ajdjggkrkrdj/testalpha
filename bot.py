baf7ab90549ea4d754fee57ba99a259a1170ade5
Ajdjggkrkrdj <128745808+Ajdjggkrkrdj@users.noreply.github.com>
2023-05-04 16:13:35 -0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -1288,6 +1288,7 @@ async def up(client: Client, message: Message):
 	   	return
 	try:
 	   msg = await message.reply("ℙ𝕣𝕖𝕡𝕒𝕣𝕒𝕟𝕕𝕠 𝕤𝕦𝕓𝕚𝕕𝕒...")
+	   await msg.pin()
 	   msgh = files_formatter(str(ROOT[username]["actual_root"]),username)
 	   path = str(ROOT[username]["actual_root"]+"/")+msgh[1][list]
 	   if USER[username]['host'] == 'educa':
@@ -1435,7 +1436,6 @@ async def up_revistas_api(file,usid,msg,username):
 						if filesize-1048>zipssize:
 							parts = math.ceil(filesize / zipssize)
 							await msg.edit(f"┏━━━━• **❅Preparando❅** •━━━━┓\n🧩 𝕋𝕠𝕥𝕒𝕝: **{parts} partes** a 丂凵乃丨尺\n┗━━━━•**❅🔩{USER[username]['zips']}MiB🔩❅**•━━━━┛")
-							await msg.pin()
 							files = await sevenzip(file,volume=zipssize)
 							print(24)
 							subido = 0
